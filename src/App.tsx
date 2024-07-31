1a66b8f512809aa852e06085760f8089458bc279
wongprom <>
2024-07-31 17:14:50 +0200

    Create basic structure for form and inputs.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
+// info input type"range" => https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range
 export function App() {
+  const n = 5;
+
+  const handlerSubmit = () => {};
+
   return (
-    <>
-      <h1>This is the App component</h1>
-    </>
+    <main>
+      <form onSubmit={handlerSubmit}>
+        <fieldset>
+          <legend>Add Movie</legend>
+          {/* Title (input) */}
+          <label htmlFor="title">Title</label>
+          <input type="text" name="title" />
+          {/* Rating 1 - 5 ⭐️⭐️⭐️☆☆ */}
+          <label htmlFor="rating">Rating</label>
+          <input type="range" min="0" max="5" name="rating" />
+          {[...Array(n)].map((e, i) => (
+            <span className="" key={i}>
+              ⭐️
+            </span>
+          ))}
+          {/* Genre (Select) Drama, Horror, Thriller, Action, Comedy, Western */}
+          <label htmlFor="genre">Genre</label>
+          <select name="genre">
+            <option value="" selected></option>
+            <option value="drama">Drama</option>
+            <option value="horror">Horror</option>
+            <option value="thriller">Thriller</option>
+            <option value="action">Action</option>
+            <option value="comedy">Comedy</option>
+            <option value="western">Western</option>
+          </select>
+          {/* Description (textarea) */}
+          <label htmlFor="">Description</label>
+          <textarea name="description" rows={10} cols={40} />
+        </fieldset>
+        {/* Clear Button */}
+        <button>Clear</button>
+        {/* Add Button */}
+        <button type="submit">Add</button>
+      </form>
+    </main>
   );
 }
