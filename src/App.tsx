bf7b3441cf15253078763fcbcbfa774d58443774
wongprom <>
2024-07-31 19:07:43 +0200

    Styling. Give basic styling for input fields.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import './App.css';
 // info input type"range" => https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range
 export function App() {
   const n = 5;
@@ -9,36 +10,52 @@ export function App() {
       <form onSubmit={handlerSubmit}>
         <fieldset>
           <legend>Add Movie</legend>
-          {/* Title (input) */}
-          <label htmlFor="title">Title</label>
-          <input type="text" name="title" />
+          {/*  Title (input) */}
+          <div className="title-wrapper">
+            <label htmlFor={'title'}>Title</label>
+            <input type={'text'} id={'title'} name={'title'} />
+          </div>
           {/* Rating 1 - 5 ⭐️⭐️⭐️☆☆ */}
-          <label htmlFor="rating">Rating</label>
-          <input type="range" min="0" max="5" name="rating" />
-          {[...Array(n)].map((e, i) => (
-            <span className="" key={i}>
-              ⭐️
-            </span>
-          ))}
+          <div className="rating-wrapper">
+            <label htmlFor={'rating'}>Rating</label>
+            <input
+              type={'range'}
+              id={'rating'}
+              name={'rating'}
+              min={'0'}
+              max={'5'}
+            />
+            <div>
+              {[...Array(n)].map((e, i) => (
+                <span key={i}>⭐️</span>
+              ))}
+            </div>
+          </div>
           {/* Genre (Select) Drama, Horror, Thriller, Action, Comedy, Western */}
-          <label htmlFor="genre">Genre</label>
-          <select name="genre">
-            <option value="" selected></option>
-            <option value="drama">Drama</option>
-            <option value="horror">Horror</option>
-            <option value="thriller">Thriller</option>
-            <option value="action">Action</option>
-            <option value="comedy">Comedy</option>
-            <option value="western">Western</option>
-          </select>
+          <div className="genre-wrapper">
+            <label htmlFor="genre">Genre</label>
+            <select id="genre" name="genre">
+              <option value=""></option>
+              <option value="drama">Drama</option>
+              <option value="horror">Horror</option>
+              <option value="thriller">Thriller</option>
+              <option value="action">Action</option>
+              <option value="comedy">Comedy</option>
+              <option value="western">Western</option>
+            </select>
+          </div>
           {/* Description (textarea) */}
-          <label htmlFor="">Description</label>
-          <textarea name="description" rows={10} cols={40} />
+          <div className="description-wrapper">
+            <label htmlFor="description">Description</label>
+            <textarea id="description" name="description" rows={10} cols={40} />
+          </div>
         </fieldset>
         {/* Clear Button */}
-        <button>Clear</button>
         {/* Add Button */}
-        <button type="submit">Add</button>
+        <div>
+          <button>Clear</button>
+          <button type="submit">Add</button>
+        </div>
       </form>
     </main>
   );
