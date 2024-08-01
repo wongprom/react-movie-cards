f416253e4ab43e08ed9100bb465cb2ec50a92326
wongprom <>
2024-08-01 17:49:59 +0200

    Validate title,genre. Disable button.
    Display error text if user first type in title input and rigt after delete all text. Error text disapere if user types again. The same behavior for genre select. Make Add button disabled if title and genre are empty string.

--- src/components/AddMovie.tsx
+++ src/components/AddMovie.tsx
@@ -9,6 +9,10 @@ interface IAddMovieProps {
 }
 
 export const AddMovie = ({ setNewMovie, addMovieToList }: IAddMovieProps) => {
+  const [errorText, setErrorText] = useState({
+    title: '',
+    genre: '',
+  });
   const [formData, setFormData] = useState({
     title: '',
     rating: '3',
@@ -17,9 +21,12 @@ export const AddMovie = ({ setNewMovie, addMovieToList }: IAddMovieProps) => {
   });
 
   const id4 = uuidv4();
+  const isFormValid = !!formData.genre && !!formData.title;
 
   const handlerSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
+    if (!isFormValid) return;
+
     const newMovie: IMovie = {
       ...formData,
       id: id4,
@@ -34,6 +41,38 @@ export const AddMovie = ({ setNewMovie, addMovieToList }: IAddMovieProps) => {
     HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
   > = (e) => {
     const { name, value } = e.target;
+
+    switch (name) {
+      case 'title':
+        if (value === '') {
+          setErrorText((prevState) => ({
+            ...prevState,
+            title: 'Write a title',
+          }));
+        } else {
+          setErrorText((prevState) => ({
+            ...prevState,
+            title: '',
+          }));
+        }
+        break;
+      case 'genre':
+        if (value === '') {
+          setErrorText((prevState) => ({
+            ...prevState,
+            genre: 'Choose a genre',
+          }));
+        } else {
+          setErrorText((prevState) => ({
+            ...prevState,
+            genre: '',
+          }));
+        }
+        break;
+      default:
+        break;
+    }
+
     setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: value,
@@ -63,6 +102,7 @@ export const AddMovie = ({ setNewMovie, addMovieToList }: IAddMovieProps) => {
             onChange={handlerInputChange}
             type={'text'}
           />
+          <span style={{ color: 'red' }}>{errorText.title}</span>
         </div>
         {/* Rating 1 - 5 ⭐️⭐️⭐️☆☆ */}
         <div className="rating-wrapper">
@@ -77,11 +117,11 @@ export const AddMovie = ({ setNewMovie, addMovieToList }: IAddMovieProps) => {
             step="1"
             type={'range'}
           />
-          <div>
+          {/* <div>
             {[...Array(5)].map((_, i) => (
               <span key={i}>⭐️</span>
             ))}
-          </div>
+          </div> */}
         </div>
         {/* Genre */}
         <div className="genre-wrapper">
@@ -100,6 +140,7 @@ export const AddMovie = ({ setNewMovie, addMovieToList }: IAddMovieProps) => {
             <option value="comedy">Comedy</option>
             <option value="western">Western</option>
           </select>
+          <span style={{ color: 'red' }}>{errorText.genre}</span>
         </div>
         {/* Description */}
         <div className="description-wrapper">
@@ -118,7 +159,9 @@ export const AddMovie = ({ setNewMovie, addMovieToList }: IAddMovieProps) => {
       {/* Add Button */}
       <div>
         <button onClick={handlerFormReset}>Clear</button>
-        <button type="submit">Add</button>
+        <button type="submit" disabled={!isFormValid}>
+          Add
+        </button>
       </div>
     </form>
   );
